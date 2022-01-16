7b0eae3e01e62cb0c36b186004820ae5fd4803ec
Cartoonish Villain <jedijoe93@gmail.com>
2022-01-15 22:36:19 -0500

    Adds messages to indicate the start and end of horde events.

--- src/main/java/com/cartoonishvillain/coldsnaphorde/events/Horde.java
+++ src/main/java/com/cartoonishvillain/coldsnaphorde/events/Horde.java
@@ -11,7 +11,9 @@ import com.cartoonishvillain.coldsnaphorde.entities.mobs.hordevariantmanager.Sta
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.server.level.ServerLevel;
@@ -46,22 +48,16 @@ public class Horde {
 
     public Horde(MinecraftServer server) {
         this.server = server;
-        for (ServerLevel serverWorld : server.getAllLevels()) {
-            String Dimension = serverWorld.dimension().toString();
-            //LOCKS CODE TO OVERWORLD
-//            if(Dimension.contains("overworld")) {
-//                serverWorld.getCapability(WorldCapability.INSTANCE).ifPresent(h -> {
-//                    hordeActive = h.getCooldownTicks() == -1;
-//                    if (hordeActive) {Alive = h.getAlive();}
-//                    world = serverWorld;
-//                });
-//            }
-        }
     }
 
     public void Stop() {
         this.bossInfo.setVisible(false);
         bossInfo.removeAllPlayers();
+        if(Alive <= 0) {
+            broadcast(server, new TranslatableComponent("message.coldsnaphorde.hordevictory").withStyle(ChatFormatting.AQUA));
+        } else {
+            broadcast(server, new TranslatableComponent("message.coldsnaphorde.hordedefeat").withStyle(ChatFormatting.RED));
+        }
         hordeActive = false;
         Alive = 0;
         initAlive = 0;
@@ -113,13 +109,15 @@ public class Horde {
 
             WorldCooldownComponent h = WORLDCOMPONENT.get(world);
                 h.setCooldownTicks(-1);
+
+            broadcast(server, new TranslatableComponent("message.coldsnaphorde.hordestart", serverPlayer.getDisplayName()).withStyle(ChatFormatting.AQUA));
         }
     }
 
     //So when a player dies the serverPlayer would desync which is... a fun issue
     //This checks for the desync, returns false if the player should not count any longer.
     private boolean checkIfPlayerIsStillValid(ServerPlayer serverPlayer) {
-        return serverPlayer.getHealth() != 0.0f && !serverPlayer.hasDisconnected();
+        return serverPlayer.getHealth() != 0.0f && !serverPlayer.hasDisconnected() && !serverPlayer.isRemoved();
     }
 
     public void tick() {
@@ -669,7 +667,7 @@ public class Horde {
 
 
 
-    private void broadcast(MinecraftServer server, TextComponent translationTextComponent){
+    private void broadcast(MinecraftServer server, Component translationTextComponent){
         server.getPlayerList().broadcastMessage(translationTextComponent, ChatType.CHAT, UUID.randomUUID());
     }
 
